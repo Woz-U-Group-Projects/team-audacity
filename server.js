9d99394844ecdcc435bf2bae316d78573415442f
marcelo guzman <marcelo.guzman9@yahoo.com>
2019-05-18 15:18:06 -0700

    Installed MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
+const indexRouter = require("./routes/index");
+const fruitRouter = require("./routes/fruits");
 
 const app = express();
+
+
 
 app.get('/api/customers', (req, res) => {
   const customers = [
@@ -15,3 +21,14 @@ app.get('/api/customers', (req, res) => {
 const port = 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
+
+
+var mongoDB = "mongodb://127.0.01/database";
+mongoose.connect(mongDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("connected", () => console.log('Mongoose connection open to ${mongoDB}'));
+db.on("disconnected", () => console.log("Mongoose connection disconnected"));
+db.on("error", console.error.bind(console, "Mongoose connection error:"));
+
+module.exports = app;
