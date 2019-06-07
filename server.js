2b7cf281309eab1a45a87e656dbb8aaaf6fe2392
Annie Mathew <annieofcambridge@gmail.com>
2019-06-07 01:57:33 -0500

    User API Routes & JWT Auth

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
+// Connect Database
+connectDB();
 
-  res.json(customers);
-});
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-const port = 5000;
+app.get('/', (req, res) => res.send('API Running'));
 
-app.listen(port, () => `Server running on port ${port}`);
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
