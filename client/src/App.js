4fa264fac476ebe485ef589ff3014142e4c64d1f
marcelo guzman <marcelo.guzman9@yahoo.com>
2019-06-09 19:56:22 -0700

    Landing Page changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import './App.css';
-import axios from "axios";
-import Customers from './components/customers';
 
-class Apps extends Component {
-  state = {
-    fruit: [],
-    newFruit: ""
-  };
-
-  getFruit() {
-    axios.get("http://localhost:5000/fruit").then(res => {
-      const fruit = res.data;
-      this.setState({ fruit });
-    });
-  }
-  componentDidMount() {
-    this.getFruit();
-  }
-  submitFruit() {
-    axios
-      .post("http://localhost:5000/fruit", { name: this.state.newFruit })
-      .then(res => {
-        this.setState({ newFruit: "" });
-        this.getFruit();
-      });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Fruits:</h1>
-        <ul>
-          {this.state.fruit.map(f => (
-            <li key={f.id}>{f.name}</li>
-          ))}
-        </ul>
-
-        <input
-          value={this.state.newFruit}
-          onChange={evt => this.setState({ newFruit: evt.target.value })}
-        />
-        <button onClick={() => this.submitFruit()}>Submit</button>
-      </div>
-    );
-  }
-}
-
-
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Story Arcadia</h1>
-        </header>
-        <Customers />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <Landing />
+  </Fragment>
+);
 
 export default App;
-
-
-
